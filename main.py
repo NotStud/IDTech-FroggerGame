6aae91904b05a1343ca05057f855e6613a128baa
NotStud <dylandcro@gmail.com>
2023-08-03 13:11:00 -0400

    renamed:    Chainsaw Doggie!.png -> ChainSawDog.png 	deleted:    FrogGame.py 	new file:   frog.py 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+from frog import Frog
+import pygame, sys
+
+pygame.init()
+pygame.event.set_allowed([pygame.KEYDOWN, pygame.QUIT])
+
+SCREEN_DIM = WIDTH, HEIGHT = 600, 500
+SCREEN = pygame.display.set_mode(SCREEN_DIM)
+
+CLOCK = pygame.time.Clock()
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (28, 128, 28)
+YELLOW = (100, 85, 0)
+BROWN = (118, 92, 72)
+GRAY = (175, 175, 175)
+BLUE = (0, 0, 175)
+
+frog = Frog()
+
+while True:
+
+  CLOCK.tick(FPS)
+  SCREEN.fill(BLACK)
+
+  SCREEN.blit(frog.image, frog.rect)
+
+  pygame.display.flip()
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_w:  # W
+        frog.move_up()
+      if event.key == pygame.K_a:  # A
+        frog.move_left()
+      if event.key == pygame.K_s:  # S
+        frog.move_down()
+      if event.key == pygame.K_d:  # D
+        frog.move_right()
+
+pygame.quit()
+
+pygame.quit()
